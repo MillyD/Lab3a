534ee2e0cccbee8923ed93e5b1b8f31caf0fb859
Emilia Duda <emilia.duda1995@gmail.com>
2022-05-15 17:18:42 +0200

    Modyfikacja TestReadTextEnc, użycie PrintStream

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/console/TestReadTextEnc.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/console/TestReadTextEnc.java
@@ -7,15 +7,17 @@ import java.time.Instant;
 public class TestReadTextEnc {
     public static void main(String[] args) throws IOException {
         BufferedReader in = null;
-        BufferedWriter out = null;;
+        BufferedWriter out = null;
 
         try {
             in = new BufferedReader(new FileReader("dzieweczki.txt", Charset.forName("windows-1250")));
-            out = new BufferedWriter(new FileWriter("dzieweczki_java.txt"));
+            //out = new BufferedWriter(new FileWriter("dzieweczki_java.txt"));
+            PrintStream out2 = new PrintStream(new FileOutputStream("dzieweczki_java.txt"));
             String line;
             while ((line = in.readLine()) != null) {
-                out.write(line);
-                out.newLine();
+                //out.write(line);
+                //out.newLine();
+                out2.println(line);
             }
         } catch (
                 IOException e) {
